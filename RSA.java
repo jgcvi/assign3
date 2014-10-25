5ad6786914c978179418a9b223826edd03abcfec
kusaibito <coatsj11@gmail.com>
2014-10-25 11:39:27 -0700

    part II

---
+++ RSA.java
@@ -0,0 +1,61 @@
+public static void main(String []args) {
+	if(args.length < 3)
+		exit();
+
+	else if(args.length == 6)
+	{
+		String[] arguments = {null, null};
+		int i;
+		for(i = 3; i < 6; i += 2)
+		{
+			if(args[i].equals("-k"))
+				arguments[0] = args[i+1];
+			else if(args[i].equals("-b"))
+				arguments[1] = args[i+1];
+		}
+		
+		if(arguments[0] == null || arguments[1] == null)
+			exit();
+		else generateKey(arguments);
+
+	} else if(args.length == 8)
+	{
+		boolean encrypt = false;
+		int i;
+		String[] arguments = {null, null, null};
+		for(i = 3; i < 8; i += 2)
+		{
+			if(args[i].equals("-e"))
+			{
+				encrypt = true;
+				arguments[0] = args[i+1];
+			} else if(args[i].equals("-i"))
+				arguments[1] = args[i+1];
+			else if(args[i].equals("-o"))
+				arguments[2] = args[i+1];
+		}
+
+		for(String el : arguments)
+			if(el == null)
+				exit();
+
+		if(encrypt)
+			encrypt(arguments);
+		else decrypt(arguments);
+	}
+
+	else exit();
+}
+
+void exit() {
+	System.out.println("Type 'java RSA -h' for help\n");
+	System.exit(1);
+}
+
+static void encrypt(String[] args) {
+
+}
+
+static void decrypt(String[] args) {
+
+}
