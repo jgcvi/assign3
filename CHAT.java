3bea0c08eefeded748f54cb0f186064d3cb4011e
kusaibito <coatsj11@gmail.com>
2014-10-29 17:41:23 -0700

    dead battery

--- CHAT.java
+++ CHAT.java
@@ -1,41 +1,42 @@
 import java.net.InetAddress;
 
 public static void main(String[] args) {
-	if(args.length != 1 || args.length != 7)
+	if(args.length != 1 || args.length != 9)
 		exit();
 
 	else if(args[0] == "-h")
 		print();
 
-	int i, port;
-	Socket s;
-	String public_key, ip;
-	for(i = 0; i < 7; i += 2)
+	boolean client = true;
+
+	for(i = 0; i < 7; i++)
 	{
-		if(args[i].equals("-e"))
-		{
-			try
-				public_key = File.open(args[i+1])
-			catch(FileNotFoundException e)
-				exit();
-		} else if(args[i].equals("-p"))
-			port = Integer.parseInt(args[i+1]);
-		else if(args[i].equals("-a"))
-			ip = args[i+1];
+		if(args[i].equals("-alice"))
+			client = false;
 	}
 
+	if(client)
+		client(args);
+
+	else server(args);
 	BufferedReader stdin, tcpin;
 	try {
 		stdin = new BufferedReader(new InputStreamReader(System.in));
-		tcpin = new BufferedReader(new InputStreamReader(s));
+		tcpstream = s.getInputStream();
+		tcpin = new BufferedReader(new InputStreamReader(tcpstream));
+
+		OutputStream osstream = s.getOutputStream();
+		PrintWriter pwrite = new PrintWriter(osstream, true);
+
+
 		e = new Socket(ip, port);
 		String input;
 		while(true)
 		{
-			if(input = stdin.readLine() != null)
-				tcpWrite(s, input);
-			else if(input = s.readLine() != null)
-				System.out.println(input);
+			if(thruPut = stdin.readLine() != null)
+				tcpWrite(s, thruPut);
+			else if(thruPut = osstream != null)
+				System.out.println(thruPut);
 		}
 
 }
@@ -47,7 +48,89 @@ void exit() {
 
 void print() {
 	System.out.println("blah");
-	exit(1);
+	exit(0);
+}
+
+static void client(String[] args) {
+	int i, port;
+	Socket s;
+	String public_key, ip;
+	InputStream tcpstream;
+	
+	boolean proper = false;
+	for(int i = 0; i < 7; i += 2)
+	{
+		if(args[i].equals("-bob"))
+		{
+			proper = true;
+			i--;
+		} else if(args[i].equals("-e"))
+		{
+			try
+				public_key = File.open(args[i+1])
+			catch(FileNotFoundException e)
+				exit();
+		} else if(args[i].equals("-d"))
+		{
+			try 
+				private_key = File.open(args[i+1]);
+			catch(FileNotFoundException e)
+				exit();
+
+		} else if(args[i].equals("-p"))
+			port = Integer.parseInt(args[i+1]);
+		
+		else if(args[i].equals("-a"))
+			ip = args[i+1];
+	}
+
+	if(!proper)
+		exit();
+
+	// START PSEUDOCODE HERE
+	generateDES(); // import DES.class
+	encryptWithPublic() // handled with RSA.java
+	send();	// handled here
+	while(1)
+	{
+		if(/*receive ok encrypted key Alice received*/)
+		{
+			startCommunications();	// handled here
+			break;
+		}
+	}
+}
+
+static void server(String[] args) {
+	for(int i = 0; i < 7; i += 2)
+	{
+		if(args[i].equals("-bob"))
+		{
+			proper = true;
+			i--;
+		} else if(args[i].equals("-e"))
+		{
+			try
+				public_key = File.open(args[i+1])
+			catch(FileNotFoundException e)
+				exit();
+		} else if(args[i].equals("-d"))
+		{
+			try 
+				private_key = File.open(args[i+1]);
+			catch(FileNotFoundException e)
+				exit();
+
+		} else if(args[i].equals("-p"))
+			port = Integer.parseInt(args[i+1]);
+		
+		else if(args[i].equals("-a"))
+			ip = args[i+1];
+	}
+}
+
+static void startCommunications() {
+
 }
 
 static void tcpWrite(Socket s, String input) {
