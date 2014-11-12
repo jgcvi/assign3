089bbf2a3379fd53e8c73423ad5d36fe35f08ef2
kusaibito <coatsj11@gmail.com>
2014-11-11 18:13:08 -0700

    DES S-boxes

--- DES.java
+++ DES.java
@@ -10,6 +10,62 @@ public class DES {
 	public static BitSet key56 = new BitSet(56);
 	static int _keyLen = 16;
 
+	static int box1[][] = new int[][]{
+		{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
+		{0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+		{4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
+		{15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
+	 };
+
+	 static int box2[][] = new int[][] {
+		{15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
+		{3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
+		{0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
+		{13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}
+	 };
+
+	 static int box3[][] = new int[][] {
+		{10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
+		{13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
+		{13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
+		{1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}
+	 };
+
+	 static int box4[][] = new int[][] {
+		{7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
+		{13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
+		{10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
+		{3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}
+	 };
+
+	 static int box5[][] = new int[][] {
+		{2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
+		{14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
+		{4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
+		{11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3}
+	 };
+
+	 static int box6[][] = new int[][] {
+		{12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
+		{10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
+		{9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
+		{4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}
+	 };
+
+	 static int box7[][] = new int[][] {
+		{4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
+		{13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
+		{1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
+		{6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}
+	 };
+
+ 	 static int box8[][] = new int[][] {
+		{13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
+		{1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
+		{7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
+		{2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}
+ 	};
+
 	public static void main(String []args) throws Exception {
 		if(args.length < 1)
 			exit();
@@ -145,6 +201,42 @@ public class DES {
 		return ret;
 	}
 
+	static int[] getIndices(BitSet lookUp) {
+		int[] ret = new int[] {0, 0};
+		if(lookUp.get(5) == true)
+			ret[0] += 2;
+		if(lookUp.get(0) == true)
+			ret[0] += 1;
+		if(lookUp.get(1) == true)
+			ret[1] += 1;
+		if(lookUp.get(2) == true)
+			ret[1] += 2;
+		if(lookUp.get(3) == true)
+			ret[1] += 4;
+
+		return ret;
+	}
+
+	static int sBoxLookUp(int box, BitSet lookUp) {
+		int[] indices = getIndices(lookUp);
+		if(box == 1)
+			return sBox1[indices[0]][indices[1]];
+		else if(box == 2)
+			return sBox2[indices[0]][indices[1]];
+		else if(box == 3)
+			return sBox3[indices[0]][indices[1]];
+		else if(box == 4)
+			return sBox4[indices[0]][indices[1]];
+		else if(box == 5)
+			return sBox5[indices[0]][indices[1]];
+		else if(box == 6)
+			return sBox6[indices[0]][indices[1]];
+		else if(box == 7)
+			return sBox7[indices[0]][indices[1]];
+		else
+			return sBox8[indices[0]][indices[1]];
+	}
+	
 	static BitSet finalPermutation(BitSet key) {
 		BitSet ret = new BitSet(64);
 		byte[] keyArr = key.toByteArray();
@@ -426,15 +518,17 @@ public class DES {
 
 		// get blocks from file
 		FileInputStream fs = openInputFile(args[1]);
-		ArrayList<String> getBlocks = getBlocks(fs);
+		ArrayList<String> blocks = getBlocks(fs);
+		fs.close();
 
 		// get keys needed
+		initial64 = BitSet.valueOf(new long[] {Long.parseLong(key)});
 		key56 = initPermutation(initial64);
 		BitSet[] keyArray = getAllKeys(key56);
 
-		applyOperations(fs, fw, keyArray);
+		// apply the perumtation functions on each block, with each key
+		encryptOperations(blocks, fw, keyArray);
 		fw.close();
-		fs.close();
 	}
 
 	/*-------------------------------------------------------------------------------------
@@ -523,6 +617,7 @@ public class DES {
 		return BitSet.valueOf(leftArr).or(BitSet.valueOf(rightArr));
 	}
 
+	// worry about this later
 	static BitSet[] getAllKeys(BitSet key) {
 		BitSet[] allKeys = new BitSet[16];
 		allKeys[0] = key;
@@ -569,6 +664,7 @@ public class DES {
 		// return keyArray;
 	}
 
+	// no bueno currently
 	static void applyOperations(FileInputStream fs, PrintWriter fw, byte[][] keyArray) {
 		ArrayList <String> blockList = new ArrayList <String>();
 		byte next;
@@ -625,6 +721,7 @@ public class DES {
 		}
 	}
 
+	// no bueno currently
 	static void decApplyOperations(FileInputStream fs, PrintWriter fw, byte[][] keyArray) {
 		ArrayList <String> blockList = new ArrayList <String>();
 		byte next;
@@ -709,7 +806,7 @@ public class DES {
 	}
 
 	static void generateInitialKey(String key) {
-		initialKey = BitSet.valueOf(new long[] {time});
+		initialKey = BitSet.valueOf(new long[] {Long.parseLong(key)});
 	}
 
 	/*-------------------------------------------------------------------------------------
